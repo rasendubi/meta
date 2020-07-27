4776f85195a5e2f9e487fa42178baf604114c7d9
Alexey Shmalko <rasen.dubi@gmail.com>
2020-07-27 21:36:52 +0300

    [meta/f] implement functions

--- meta/meta/f.rkt
+++ meta/meta/f.rkt
@@ -4,6 +4,7 @@
 (require "./base.rkt")
 (require "./core.rkt")
 
+;; TODO: remove hard-code and find these by identifiers
 (define StringLiteral "100")
 (define StringLiteral.value "101")
 (define RecordLiteral "102")
@@ -14,6 +15,29 @@
 (define FieldAccess "107")
 (define FieldAccess.record "108")
 (define FieldAccess.field "109")
+(define Function "110")
+(define Function.parameter "111")
+(define Function.body "112")
+(define FunctionParameter "113")
+(define FunctionParameter.name "114")
+(define Apply "115")
+(define Apply.function "116")
+(define Apply.argument "117")
+(define Reference "118")
+(define Reference.reference "119")
+
+(define f-environment (make-parameter '()))
+
+(define (f-environment-lookup id)
+  (let ([ctx (findf (lambda (x) (hash-has-key? x id))
+                    (f-environment))])
+    (if ctx
+        (hash-ref ctx id)
+        (error 'f-context-lookup "Unable to find ~a, environment = ~v" id (f-environment)))))
+
+(define-syntax-rule (with-environment env body ...)
+  (parameterize ([f-environment (cons env (f-environment))])
+    body ...))
 
 (provide f-eval)
 (define (f-eval meta id)
@@ -22,6 +46,9 @@
     [(("102")) (eval-record meta id)]
     [(("104")) (eval-field meta id)]
     [(("107")) (eval-field-access meta id)]
+    [(("110")) (eval-function meta id)]
+    [(("115")) (eval-apply meta id)]
+    [(("118")) (eval-reference meta id)]
     [else (error 'f-eval "Unable to evaluate ~a" id)]))
 
 (define (eval-string meta id)
@@ -43,6 +70,26 @@
          [field     (f-eval meta field-id)])
     (hash-ref record field)))
 
+(struct f-function (parameter-id body-id) #:transparent)
+
+(define (eval-function meta id)
+  (let ([parameter-id (meta-lookup-val meta id Function.parameter)]
+        [body-id      (meta-lookup-val meta id Function.body)])
+    (f-function parameter-id body-id)))
+
+(define (eval-apply meta id)
+  (let ([function-id (meta-lookup-val meta id Apply.function)]
+        [argument-id (meta-lookup-val meta id Apply.argument)])
+    (f-apply meta (f-eval meta function-id) (f-eval meta argument-id))))
+
+(define (f-apply meta function argument)
+  (with-environment (make-immutable-hash (list (cons (f-function-parameter-id function) argument)))
+    (f-eval meta (f-function-body-id function))))
+
+(define (eval-reference meta id)
+  (let ([reference-id (meta-lookup-val meta id Reference.reference)])
+    (f-environment-lookup reference-id)))
+
 (provide f-print)
 (define (f-print x)
   (println x))
@@ -55,7 +102,13 @@
     [(("102")) (pretty-record meta id)]
     [(("104")) (pretty-field meta id)]
     [(("107")) (pretty-field-access meta id)]
-    [else (error 'f-pretty "Unable to pretty-print ~a" id)]))
+    [(("110")) (pretty-function meta id)]
+    [(("113")) (pretty-function-parameter meta id)]
+    [(("115")) (pretty-apply meta id)]
+    [(("118")) (pretty-reference meta id)]
+    [else (h-append (text "(") (text id) (text ")"))]
+    ;; [else (error 'f-pretty "Unable to pretty-print ~a" id)]
+    ))
 
 (define (pretty-string meta id)
   (let ([val (meta-lookup-val meta id StringLiteral.value)])
@@ -63,34 +116,64 @@
 
 (define (pretty-record meta id)
   (let ([field-ids (meta-lookup-vals meta id RecordLiteral.field)])
-    (group (h-append (text "{")
-                     (nest 2 (h-append
-                              line
-                              (h-concat (apply-infix (h-append comma line)
-                                                     (map (lambda (x) (f-pretty meta x)) field-ids)))))
-                     line
-                     (text "}")))))
+    (group
+     (h-append
+      (text "{")
+      (nest 2 (h-append
+               line
+               (h-concat
+                (apply-infix (h-append comma line) (map (lambda (x) (f-pretty meta x)) field-ids)))))
+      line
+      (text "}")))))
 
 (define (pretty-field meta id)
   (let ([key-id (meta-lookup-val meta id RecordField.key)]
         [value-id (meta-lookup-val meta id RecordField.value)])
-    (group
-     (h-append
-      (group (h-append (text "[") (f-pretty meta key-id)   (text "]") (text ":")))
-      (nest 2
-            (h-append
-             line
-             (f-pretty meta value-id)))))))
+    (group (nest 2 (h-append
+                    (group (h-append (text "[") (f-pretty meta key-id) (text "]") (text ":")))
+
+                    line
+                    (f-pretty meta value-id))))))
 
 (define (pretty-field-access meta id)
   (let ([record-id (meta-lookup-val meta id FieldAccess.record)]
         [field-id  (meta-lookup-val meta id FieldAccess.field)])
-    (group (h-append
-            (f-pretty meta record-id)
-            (nest 2
-                  (h-append
-                   break
-                   dot
-                   (text "[")
-                   (f-pretty meta field-id)
-                   (text "]")))))))
+    (group (nest 2
+                 (h-append
+                  (f-pretty meta record-id)
+                  break
+                  dot
+                  (text "[")
+                  (f-pretty meta field-id)
+                  (text "]"))))))
+
+(define (pretty-function meta id)
+  (let ([parameter-id (meta-lookup-val meta id Function.parameter)]
+        [body-id      (meta-lookup-val meta id Function.body)])
+    (group (nest 2 (h-append
+                    (text "\\")
+                    (f-pretty meta parameter-id)
+                    (text " ")
+                    (text "->")
+                    line
+                    (f-pretty meta body-id))))))
+
+(define (pretty-function-parameter meta id)
+  (let ([name (meta-lookup-val meta id FunctionParameter.name)])
+    (text name)))
+
+(define (pretty-apply meta id)
+  (let ([function-id (meta-lookup-val meta id Apply.function)]
+        [argument-id (meta-lookup-val meta id Apply.argument)])
+    (group (nest 2 (h-append
+                    (text "(")
+                    (f-pretty meta function-id)
+                    (text ")")
+                    line
+                    (text "(")
+                    (f-pretty meta argument-id)
+                    (text ")"))))))
+
+(define (pretty-reference meta id)
+  (let ([reference-id (meta-lookup-val meta id Reference.reference)])
+    (f-pretty meta reference-id)))
