6f5d4cbcecfe996e3f51f931e9c35c07230fe7bd
Alexey Shmalko <rasen.dubi@gmail.com>
2020-10-11 12:32:47 +0300

    [meta-gui][scrollbar] add vertical padding

--- meta-rs/meta-gui/src/widgets/scrollbar.rs
+++ meta-rs/meta-gui/src/widgets/scrollbar.rs
@@ -24,8 +24,11 @@ impl Layout for Scrollbar {
     fn layout(&mut self, ctx: &mut GuiContext, constraint: Constraint) -> Size {
         let size = constraint.min;
 
-        let scrollbar_height = self.height * size.height;
-        let scrollbar_offset = self.offset * size.height;
+        let pad_height = 1.0;
+        let effective_height = size.height - 2.0 * pad_height;
+
+        let scrollbar_height = self.height * effective_height;
+        let scrollbar_offset = self.offset * effective_height;
         let background = ctx.solid_brush(Color::rgb8(0xe0, 0xe0, 0xe0));
         let foreground = ctx.solid_brush(Color::rgb8(0x42, 0x42, 0x42));
 
@@ -33,7 +36,7 @@ impl Layout for Scrollbar {
         ctx.fill(
             Rect::new(
                 1.0,
-                scrollbar_offset,
+                scrollbar_offset + pad_height,
                 size.width - 1.0,
                 scrollbar_offset + scrollbar_height,
             )
