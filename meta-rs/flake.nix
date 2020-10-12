ae01b0c9e07033bbccc211d5cb5fff2ae1afb88b
Alexey Shmalko <rasen.dubi@gmail.com>
2020-10-12 11:01:54 +0300

    [meta-rs][nix] sort out nativeBuildInputs/buildInputs, add cargo audit

--- meta-rs/flake.nix
+++ meta-rs/flake.nix
@@ -5,7 +5,7 @@
     devShell.x86_64-linux =
       let pkgs = nixpkgs.legacyPackages.x86_64-linux;
       in pkgs.mkShell {
-        buildInputs = [
+        nativeBuildInputs = [
           pkgs.rustc
           pkgs.cargo
           pkgs.rls
@@ -16,7 +16,13 @@
           pkgs.rustup
           pkgs.rust-analyzer
 
+          # for cargo-audit
+          pkgs.pkg-config
+        ];
+        buildInputs = [
           pkgs.gtk3
+          # for cargo-audit
+          pkgs.openssl
         ];
 
         RUST_BACKTRACE = "1";
